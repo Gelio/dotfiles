d1551e7d7c2899595d14b8d895e031e2bf321b26
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-10-18 15:48:28 +0200

    feat(nvim): use gpt-5-mini model for all codecompanion strategies

--- universal/neovim/config/nvim/lua/plugins/editing/copilot.lua
+++ universal/neovim/config/nvim/lua/plugins/editing/copilot.lua
@@ -48,30 +48,40 @@ return {
 			"nvim-lua/plenary.nvim",
 			mcphub[1],
 		},
-		opts = {
-			strategies = {
-				chat = {
-					adapter = {
-						name = "copilot",
-						-- NOTE: according to Reddit, gpt-5-mini performs better than gpt-4.1
-						model = "gpt-5-mini",
+		opts = function()
+			local adapter = {
+				name = "copilot",
+				-- NOTE: according to Reddit, gpt-5-mini performs better than gpt-4.1
+				model = "gpt-5-mini",
+			}
+
+			return {
+				strategies = {
+					chat = {
+						adapter = adapter,
+					},
+					inline = {
+						adapter = adapter,
+					},
+					cmd = {
+						adapter = adapter,
 					},
 				},
-			},
-			extensions = {
-				mcphub = {
-					callback = "mcphub.extensions.codecompanion",
-					opts = {
-						make_tools = true,
-						show_server_tools_in_chat = true,
-						add_mcp_prefix_to_tool_names = true,
-						show_result_in_chat = true,
-						make_vars = true,
-						make_slash_commands = true,
+				extensions = {
+					mcphub = {
+						callback = "mcphub.extensions.codecompanion",
+						opts = {
+							make_tools = true,
+							show_server_tools_in_chat = true,
+							add_mcp_prefix_to_tool_names = true,
+							show_result_in_chat = true,
+							make_vars = true,
+							make_slash_commands = true,
+						},
 					},
 				},
-			},
-		},
+			}
+		end,
 		cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions", "CodeCompanionCmd" },
 	},
 }
