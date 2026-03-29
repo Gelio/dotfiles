59c6af9dd511602a395028b47c9c4e7da9345354
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2026-03-29 13:36:14 +0200

    feat(nvim): add a way to customize mole.nvim session file name

--- universal/neovim/config/nvim/lua/plugins/extra/mole.lua
+++ universal/neovim/config/nvim/lua/plugins/extra/mole.lua
@@ -5,6 +5,15 @@ return {
 			"MunifTanjim/nui.nvim",
 		},
 		cmd = { "MoleStart", "MoleResume", "MoleToggle" },
-		opts = {},
+		opts = {
+			session_name = function()
+				local basename = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+				local desc = vim.fn.input("Session description (default: " .. basename .. "): ")
+				if desc == "" then
+					desc = basename
+				end
+				return "session_" .. os.date("%Y-%m-%d_%H-%M-%S") .. "-" .. desc
+			end,
+		},
 	},
 }
