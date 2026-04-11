0feb4c5db8d4d4611df51e91cc3aba8fa6cfa4e5
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2026-04-11 17:00:43 +0200

    chore(nvim): remove nvim-FeMaco.lua
    The plugin does not work anymore with the latest nvim-treesitter.

--- universal/neovim/config/nvim/lua/plugins/lang/markdown.lua
+++ universal/neovim/config/nvim/lua/plugins/lang/markdown.lua
@@ -9,35 +9,6 @@ return {
 	},
 
 	{
-		"AckslD/nvim-FeMaco.lua",
-		cmd = { "FeMaco" },
-		opts = {
-			ft_from_lang = function(lang)
-				if lang == "dataviewjs" then
-					return "javascript"
-				end
-
-				return lang
-			end,
-
-			create_tmp_filepath = function(filetype)
-				local extension = ""
-				if filetype == "javascript" then
-					extension = ".js"
-				elseif filetype == "lua" then
-					extension = ".lua"
-				end
-
-				return os.tmpname() .. extension
-			end,
-
-			ensure_newline = function(_base_filetype)
-				return true
-			end,
-		},
-	},
-
-	{
 		"mzlogin/vim-markdown-toc",
 		ft = "markdown",
 	},
