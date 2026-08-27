f47bdf0b922448437c1f709b1b26d00264dc3005
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2026-08-27 11:28:33 +0200

    refactor(git): install many utilities using single cargo binstall cmd
    Parallelizes gathering packages metadata and requires fewer confirmation
    prompts.

--- universal/git/utilities.sh
+++ universal/git/utilities.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-# UI for git
-cargo binstall gitui
+# https://github.com/epage/git-stack
+# https://github.com/Wilfred/difftastic
+# https://github.com/tummychow/git-absorb
+cargo binstall gitui git-stack difftastic git-absorb
 
 # https://github.com/arxanas/git-branchless
 cargo binstall --locked git-branchless
 
-# https://github.com/epage/git-stack
-cargo binstall git-stack
-
-# https://github.com/Wilfred/difftastic
-cargo binstall difftastic
-
 # https://github.com/jesseduffield/lazygit
 go install github.com/jesseduffield/lazygit@latest
-
-# https://github.com/tummychow/git-absorb
-cargo binstall git-absorb
